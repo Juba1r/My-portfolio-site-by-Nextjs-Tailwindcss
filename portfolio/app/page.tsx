44401880df026b8ffaff5e3ba4d2619861260d83
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-12-31 00:24:24 +0600

    starting

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="relative bg-black">
+      <div>
+        <h1 className="text-blue-200">Hello , Portfolio</h1>
+      </div>
+    </main>
+  );
+}
