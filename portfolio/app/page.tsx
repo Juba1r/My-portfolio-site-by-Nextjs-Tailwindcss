317e93ad22fb167b27020edb6a1665e5eedab421
Jubair Ibn Khaled <jubairibnkhaled@gmail.com>
2025-03-05 12:54:38 +0600

    update 1

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="relative bg-black">
+    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto ">
       <div>
         <h1 className="text-blue-200">Hello , Portfolio</h1>
       </div>
